3799805a48c69ed6dc2179cab9fa27c31f79ca3a
Taeksoo <jazzsaxmafia@gmail.com>
2014-09-21 14:10:40 +0900

    first

---
+++ grade_compute.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*
+
+fin= open("Untitled.csv")
+fout = open("output.txt", "w");
+score_dic = {
+                "A+":4.5,
+                "A" :4.0,
+                "B+":3.5,
+                "B" :3.0,
+                "C+":2.5,
+                "C" :2.0
+            };
+
+score_sum = 0;
+num_major = 0;
+isu = 0;
+while 1:
+    one_line = fin.readline();
+    if not one_line: break;
+
+    split_line = one_line.split("\t");
+
+
+    if split_line[4].find("전공") != -1 and split_line[9] != 'F' :
+        print one_line
+        score_sum += score_dic[split_line[9]];
+        num_major += 1;
+        isu += int(split_line[7]);
+
+print score_sum
+print num_major
+print isu
+
+
